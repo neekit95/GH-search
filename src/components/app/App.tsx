a9f7372991d15b38268009b861732eab6c5604bf
neekit95 <nikita-ribalkin@mail.ru>
2024-08-07 17:36:29 +0300

    Add redux, MUI, SASS. Restructured some files

---
+++ src/components/app/App.tsx
@@ -0,0 +1,12 @@
+import style from './app.module.scss';
+
+const App = () => {
+    return (
+        <div className={style.container}>
+            app
+        </div>
+    );
+};
+
+export default App;
+
