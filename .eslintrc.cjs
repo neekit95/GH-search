986b8beccaf2ea44c711e564c19345e8d9874520
neekit95 <nikita-ribalkin@mail.ru>
2024-08-07 17:45:20 +0300

    eslint change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	env: {browser: true, es2020: true},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin :react/recommended',
+		'plugin:react/jsx-runtime'
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+		tsconfigRootDir: __dirname,
+	},
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{allowConstantExport: true},
+		],
+	},
 }
