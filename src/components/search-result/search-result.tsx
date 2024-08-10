0fa9bbc71a0c7c319db72eab09c38906bbcb72ae
neekit95 <nikita-ribalkin@mail.ru>
2024-08-10 14:24:44 +0300

    loading without error

--- src/components/search-result/search-result.tsx
+++ src/components/search-result/search-result.tsx
@@ -9,14 +9,6 @@ import MenuItem from '@mui/material/MenuItem';
 import debounce from 'lodash.debounce';
 import style from './search-result.module.scss';
 
-interface License {
-	key: string;
-	name: string;
-	spdx_id: string;
-	url: string;
-	node_id: string;
-}
-
 interface RowData {
 	id: string;
 	name: string;
@@ -25,7 +17,7 @@ interface RowData {
 	stargazers_count: number;
 	updated_at: string;
 	description: string;
-	license: License | string | null;
+	license: string | null;
 	isChosen: boolean;
 }
 
@@ -51,41 +43,54 @@ interface SearchResultProps {
 
 const SearchResult: React.FC<SearchResultProps> = ({ filter }) => {
 	const dispatch = useDispatch<AppDispatch>();
-	const { items: repositories, status } = useSelector((state: RootState) => state.repositories);
+	const { items: repositories } = useSelector((state: RootState) => state.repositories);
 	const [paginationCount, setPaginationCount] = useState<number>(10);
 	const [currentPage, setCurrentPage] = useState<number>(1);
 	const [displayedRows, setDisplayedRows] = useState<RowData[]>([]);
 	const [hasMore, setHasMore] = useState<boolean>(true);
 	const [chosenRepoId, setChosenRepoId] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<boolean>(false);
+	const [noResults, setNoResults] = useState<boolean>(false);
 
 	// Дебаунсинг функции запроса данных
 	const fetchRepositoriesDebounced = useCallback(debounce((filter: string, page: number) => {
+		setLoading(true);
+		setError(false);
+		setNoResults(false);
 		dispatch(fetchRepositories({ query: filter, perPage: 100, page }))
 			.then((response) => {
 				const payload = response.payload as Repository[];
+				setLoading(false);
 				if (payload.length === 0) {
+					setNoResults(true);
 					setHasMore(false);
+				} else {
+					setHasMore(true);
+					setNoResults(false);
 				}
 			})
 			.catch(() => {
-				console.log('Error fetching more data');
+				setLoading(false);
+				setError(true);
 				setHasMore(false);
+				setNoResults(false);
 			});
 	}, 500), [dispatch]);
 
 	useEffect(() => {
 		if (filter) {
 			dispatch(clearRepositories());
-			setPaginationCount(10); // Сброс значения пагинации
-			setCurrentPage(1); // Сброс текущей страницы
-			setHasMore(true); // Обновление состояния hasMore
-			setChosenRepoId(''); // Сброс выбранного репозитория
+			setPaginationCount(10);
+			setCurrentPage(1);
+			setHasMore(true);
+			setChosenRepoId('');
 			fetchRepositoriesDebounced(filter, 1);
 		}
 	}, [filter, dispatch, fetchRepositoriesDebounced]);
 
 	useEffect(() => {
-		if (repositories) {
+		if (repositories.length > 0 && !loading && !error) {
 			const startIndex = (currentPage - 1) * paginationCount;
 			const endIndex = startIndex + paginationCount;
 			const slicedRepositories = repositories.slice(startIndex, endIndex);
@@ -96,7 +101,7 @@ const SearchResult: React.FC<SearchResultProps> = ({ filter }) => {
 				fetchRepositoriesDebounced(filter, nextPage);
 			}
 		}
-	}, [repositories, currentPage, paginationCount, fetchRepositoriesDebounced, filter, hasMore, chosenRepoId]);
+	}, [repositories, currentPage, paginationCount, fetchRepositoriesDebounced, filter, hasMore, chosenRepoId, loading, error]);
 
 	const handleRowsPerPageChange = (event: SelectChangeEvent<number>) => {
 		const newPaginationCount = Number(event.target.value);
@@ -119,87 +124,73 @@ const SearchResult: React.FC<SearchResultProps> = ({ filter }) => {
 		setChosenRepoId(prevChosenRepoId => prevChosenRepoId === repoId ? '' : repoId);
 	};
 
-	const renderLicense = (license: License | string | null) => {
-		if (license && typeof license === 'object') {
-			return license.name;
-		} else if (typeof license === 'string') {
-			return license;
-		} else {
-			return 'No license available';
-		}
-	};
-
 	const chosenRepoDetails = repositories.find(repo => repo.id === chosenRepoId);
 
 	return (
 		<div className={style.container}>
-			{status === 'loading' ? (
+			{loading ? (
 				<Loading />
-			) : status === 'failed' ? (
+			) : error ? (
 				<div className={style.nothing}>Ошибка загрузки данных. Пожалуйста, попробуйте позже.</div>
+			) : noResults ? (
+				<div className={style.nothing}>Ничего не найдено, повторите поиск</div>
 			) : (
-				<>
-					{repositories.length === 0 ? (
-						<div className={style.nothing}>Ничего не найдено, повторите поиск</div>
-					) : (
-						<div className={style.main}>
-							<div className={style.left}>
-								<h1>Результаты поиска</h1>
-								<div className={style.table}>
-									<DataTable
-										rows={displayedRows}
-										onRowClick={handleRowClick}
-									/>
-								</div>
-								<div className={style.pagination}>
-									<div className={style.paginationRow}>
-										<p>Rows per page:</p>
-										<Select
-											value={paginationCount}
-											onChange={handleRowsPerPageChange}
-											variant="standard"
-											className={style.select}
-										>
-											{itemsPerPageOptions.map(option => (
-												<MenuItem key={option} value={option}>
-													{option}
-												</MenuItem>
-											))}
-										</Select>
-									</div>
-									<div className={style.paginationElements}>
-										{(currentPage - 1) * paginationCount + 1} - {Math.min(currentPage * paginationCount, repositories.length)} of {repositories.length}
-									</div>
-									<div className={style.paginationButtons}>
-										<button
-											disabled={currentPage === 1}
-											onClick={() => handlePageChange('prev')}
-										>
-											&lt;
-										</button>
-										<button
-											disabled={currentPage === Math.ceil(repositories.length / paginationCount) && !hasMore}
-											onClick={() => handlePageChange('next')}
-										>
-											&gt;
-										</button>
-									</div>
-								</div>
+				<div className={style.main}>
+					<div className={style.left}>
+						<h1>Результаты поиска</h1>
+						<div className={style.table}>
+							<DataTable
+								rows={displayedRows}
+								onRowClick={handleRowClick}
+							/>
+						</div>
+						<div className={style.pagination}>
+							<div className={style.paginationRow}>
+								<p>Rows per page:</p>
+								<Select
+									value={paginationCount}
+									onChange={handleRowsPerPageChange}
+									variant="standard"
+									className={style.select}
+								>
+									{itemsPerPageOptions.map(option => (
+										<MenuItem key={option} value={option}>
+											{option}
+										</MenuItem>
+									))}
+								</Select>
 							</div>
-							{chosenRepoDetails && (
-								<div className={style.right}>
-									<h2>{chosenRepoDetails.name}</h2>
-									<p><strong>Language:</strong> {chosenRepoDetails.language}</p>
-									<p><strong>Forks:</strong> {chosenRepoDetails.forks_count}</p>
-									<p><strong>Stars:</strong> {chosenRepoDetails.stargazers_count}</p>
-									<p><strong>Last Updated:</strong> {chosenRepoDetails.updated_at}</p>
-									<p><strong>Description:</strong> {chosenRepoDetails.description}</p>
-									<p><strong>License:</strong> {renderLicense(chosenRepoDetails.license)}</p>
-								</div>
-							)}
+							<div className={style.paginationElements}>
+								{(currentPage - 1) * paginationCount + 1} - {Math.min(currentPage * paginationCount, repositories.length)} of {repositories.length}
+							</div>
+							<div className={style.paginationButtons}>
+								<button
+									disabled={currentPage === 1}
+									onClick={() => handlePageChange('prev')}
+								>
+									&lt;
+								</button>
+								<button
+									disabled={currentPage === Math.ceil(repositories.length / paginationCount) && !hasMore}
+									onClick={() => handlePageChange('next')}
+								>
+									&gt;
+								</button>
+							</div>
+						</div>
+					</div>
+					{chosenRepoDetails && (
+						<div className={style.right}>
+							<h2>{chosenRepoDetails.name}</h2>
+							<p><strong>Language:</strong> {chosenRepoDetails.language}</p>
+							<p><strong>Forks:</strong> {chosenRepoDetails.forks_count}</p>
+							<p><strong>Stars:</strong> {chosenRepoDetails.stargazers_count}</p>
+							<p><strong>Last Updated:</strong> {chosenRepoDetails.updated_at}</p>
+							<p><strong>Description:</strong> {chosenRepoDetails.description}</p>
+							<p><strong>License:</strong> {chosenRepoDetails.license}</p>
 						</div>
 					)}
-				</>
+				</div>
 			)}
 		</div>
 	);
